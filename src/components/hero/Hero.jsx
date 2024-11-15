f348295038e8117ca1ce5700eab2ebaf60b9df87
Suraj Mishra <mishrasur7@gmail.com>
2024-11-15 16:46:22 +0200

    download made

--- src/components/hero/Hero.jsx
+++ src/components/hero/Hero.jsx
@@ -17,9 +17,9 @@ export const Hero = () => {
             <div className='lg:flex-grow md:w-1/2 lg:pr-24 md:pr-16 flex flex-col md:items-start md:text-left
             mb-16 md:mb-0 items-center text-center relative'>
                 <h1 className='title-font sm:text-4xl text-4xl font-bold text-white'>Hi! I&apos;m Suraj Mishra</h1>
-                <p className='my-5 leading-relaxed'>I am a passionate Software Developer.</p>
+                <p className='my-5 leading-relaxed'>Passionate Software Developer.</p>
                 <div className='flex justify-center'>
-                    <a href={CV}>
+                    <a href={CV} download>
                         <button className='inline-flex text-white bg-orange-500 border-0 py-2 px-6
                         focus:outline-none hover:bg-orange-600 hover:shadow-[0_0_40px_rgb(255,165,0,0.7)]
                         rounded-full text-lg'>
