071e5496a086e866de145b1c82399bdece7d878c
Suraj Mishra <mishrasur7@gmail.com>
2024-11-15 11:11:43 +0200

    cv added for download

--- src/components/hero/Hero.jsx
+++ src/components/hero/Hero.jsx
@@ -1,4 +1,5 @@
 import Navbar from "../navbar/Navbar"
+import CV from '../../assets/CV_mishra_suraj.pdf'
 
 export const Hero = () => {
   return (
@@ -7,6 +8,27 @@ export const Hero = () => {
     via-yellow-500 to-red-400 absolute rounded-full transform rotate-12 -top-20 shadow-[0_0_40px_rgb(255, 165, 0, 0.7)]'>
     </div>
     <Navbar />
+    <section className='text-white body-font z-10'>
+        <div className='container mx-auto flex px-5 py-24 md:flex-row flex-col items-center'>
+            <div className='lg:flex-grow md:w-1/2 lg:pr-24 md:pr-16 flex flex-col md:items-start md:text-left
+            mb-16 md:mb-0 items-center text-center relative'>
+                <h1 className='title-font sm:text-4xl text-4xl font-bold text-white'>Hi! I&apos;m Suraj Mishra</h1>
+                <p className='my-5 leading-relaxed'>I am a passionate Software Developer.</p>
+                <div className='flex justify-center'>
+                    <a href={CV}>
+                        <button className='inline-flex text-white bg-orange-500 border-0 py-2 px-6
+                        focus:outline-none hover:bg-orange-600 hover:shadow-[0_0_40px_rgb(255,165,0,0.7)]
+                        rounded-full text-lg'>
+                            Download CV
+                        </button>
+                    </a>
+
+                </div>
+            </div>
+
+        </div>
+
+    </section>
     </div>
   )
 }
