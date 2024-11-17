399f6bfd521cd5e91184a9c9ea701e36dc6347a1
Suraj Mishra <mishrasur7@gmail.com>
2024-11-17 20:56:07 +0200

    skills component added

---
+++ src/components/skills/Skills.jsx
@@ -0,0 +1,24 @@
+const Skills = () => {
+  return (
+    <section id='skills' className='relative overflow-hidden flex flex-col text-white body-font'>
+        <div className='container flex flex-wrap px-5 py-24 mx-auto items-center'>
+            <div
+            data-aos='fade-up'
+            data-aos-delay='200'
+            className='md:w-1/2 md:pr-12 md:py-8 md:border-r md:border-b-0 mb-10 md:mb-0 pd-10 border-b border-orange-50'>
+                <h1 
+                data-aos='fade-right'
+                data-aos-delay='500'
+                className='sm:text-4xl text-2xl font-medium title-font mb-2 text-white'>
+                    Skills
+                </h1>
+
+            </div>
+
+        </div>
+
+    </section>
+  )
+}
+
+export default Skills
