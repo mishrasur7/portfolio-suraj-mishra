0c1cc4535823907e0fef9cbe537528128ff397e5
Suraj Mishra <mishrasur7@gmail.com>
2024-11-19 07:04:04 +0200

    logos and experience component created

---
+++ src/components/experience/Experience.jsx
@@ -0,0 +1,13 @@
+
+const Experience = () => {
+  return (
+    <section id='experience' className='relative overflow-hidden flex flex-col text-gray-600 body-font'>
+        <div className='absolute top-[100px] left-64 inset-x-0 flex items-start justify-center'>
+
+        </div>
+
+    </section>
+  )
+}
+
+export default Experience
