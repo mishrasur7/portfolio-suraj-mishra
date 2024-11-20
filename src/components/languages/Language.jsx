74effea4a9a2842ac36c50ae059b6678f75194b4
Suraj Mishra <mishrasur7@gmail.com>
2024-11-20 22:01:04 +0200

    languages added

---
+++ src/components/languages/Language.jsx
@@ -0,0 +1,14 @@
+const Language = () => {
+    const languages = [
+        {code: 'Np', name: 'Nepali', level: 'Mother tongue'},
+        {code: 'En', name: 'English', level: 'Excellent'},
+        {code: 'Hi', name: 'Hindi', level: 'Intermediate'},
+        {code: 'Fi', name: 'Finnish', level: 'Intermediate'},
+    ]
+
+  return (
+    <div>Language</div>
+  )
+}
+
+export default Language
