5b2d02eb273d4ce56e137791468138a03286b7d0
Suraj Mishra <mishrasur7@gmail.com>
2024-11-21 21:45:06 +0200

    projects component created

---
+++ src/components/projects/Projects.jsx
@@ -0,0 +1,14 @@
+import Slider from "react-slick";
+
+const Projects = () => {
+  const projectsList = [
+    {id: 1, image: image, title: 'Project 1', description: ''}
+  ]
+  return (
+    <section >
+
+    </section>
+  )
+}
+
+export default Projects
