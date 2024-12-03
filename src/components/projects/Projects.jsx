6386719cd2377811105818affcc031a7387d927f
Suraj Mishra <mishrasur7@gmail.com>
2024-12-03 10:20:55 +0200

    projects added

--- src/components/projects/Projects.jsx
+++ src/components/projects/Projects.jsx
@@ -38,12 +38,6 @@ const Projects = () => {
       title: 'Softala Project', 
       description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, quos.'
     },
-    {
-      id: 1, 
-      image: siba, 
-      title: 'Softala Project', 
-      description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, quos.'
-    },
   ]
   return (
     <div id='projects' className='mt-24 scroll-m-20'>
@@ -51,7 +45,7 @@ const Projects = () => {
       <section className='mx-4 flex flex-wrap gap-10 justify-evenly sm:flex-row '>
         {projectsList.map((project) => {
           return (
-            <div key={project.id} className=' bg-[#6A98F0] hover:bg-yellow-500 hover:cursor-pointer rounded-xl p-4'>
+            <div key={project.id} className='p-10 bg-[#000000] hover:bg-yellow-500 hover:cursor-pointer rounded-xl'>
               <img src={project.image} alt={project.title} className='w-72 h-48'/>
               <div className='text-white mt-5'>
                 <h3 className='text-xl font-bold'>{project.title}</h3>
