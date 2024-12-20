1e14fdff595d6ca9a98ba3da2e2f0d691cd3feb1
Suraj Mishra <mishrasur7@gmail.com>
2024-12-20 19:26:12 +0200

    saas project image added

--- src/components/projects/Projects.jsx
+++ src/components/projects/Projects.jsx
@@ -1,4 +1,5 @@
 import siba from '../../assets/siba_login_view.png'
+import saas from '../../assets/saas.png'
 
 const Projects = () => {
   const projectsList = [
@@ -9,9 +10,9 @@ const Projects = () => {
       description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, quos.'
     },
     {
-      id: 1, 
-      image: siba, 
-      title: 'Softala Project', 
+      id: 2, 
+      image: saas, 
+      title: 'SAAS project', 
       description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, quos.'
     },
     {
@@ -39,8 +40,8 @@ const Projects = () => {
       <section className='mx-4 flex flex-wrap gap-10 justify-evenly sm:flex-row '>
         {projectsList.map((project) => {
           return (
-            <div key={project.id} className='p-10 border-solid border-[#6D83F2] border-b-2 border-l-2 hover:cursor-pointer rounded-xl'>
-              <img src={project.image} alt={project.title} className='w-96 h-48'/>
+            <div key={project.id} className='flex-initial w-96 p-10 border-solid border-[#6D83F2] border-b-2 border-l-2 hover:cursor-pointer rounded-xl'>
+              <img src={project.image} alt={project.title} className='w-64 h-36'/>
               <div className='text-[#6D83F2] mt-5'>
                 <h3 className='text-xl font-bold'>{project.title}</h3>
                 <p>{project.description}</p>
