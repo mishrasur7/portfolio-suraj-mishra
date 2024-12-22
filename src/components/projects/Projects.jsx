265b3f0b9c08a3a76f94159499da4ac4dd894ae9
Suraj Mishra <mishrasur7@gmail.com>
2024-12-22 13:59:38 +0200

    integrated live link to div

--- src/components/projects/Projects.jsx
+++ src/components/projects/Projects.jsx
@@ -50,13 +50,21 @@ const Projects = () => {
       github: ['https://github.com/mishrasur7/fs11-SASS/tree/sass']
     },
   ]
+
+  const handleClick = (link) => {
+    window.location.href = link
+  }
+
   return (
     <div id='projects' className='mt-24 scroll-m-20'>
       <h1 className='text-2xl text-center text-[#6D83F2] font-mono mb-10 '>Projects</h1>
       <section className='mx-4 flex flex-wrap gap-10 justify-evenly sm:flex-row '>
         {projectsList.map((project) => {
           return (
-            <div key={project.id} className='flex-initial w-96 p-7 border-solid border-[#6D83F2] border-b-2 border-l-2 hover:cursor-pointer rounded-xl'>
+            <div 
+            onClick={() => handleClick(project.liveLink)}
+            key={project.id} 
+            className='flex-initial w-96 p-7 border-solid border-[#6D83F2] border-b-2 border-l-2 hover:cursor-pointer rounded-xl'>
               <img src={project.image} alt={project.title} className='w-72 h-40'/>
               <div className='text-[#6D83F2] mt-5'>
                 <h3 className='text-xl font-bold'>{project.title}</h3>
