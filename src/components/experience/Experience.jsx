768dd1c7299bac3181869594db6dfd6eae5de597
Suraj Mishra <mishrasur7@gmail.com>
2025-01-09 14:54:28 +0200

    added more work experiences

--- src/components/experience/Experience.jsx
+++ src/components/experience/Experience.jsx
@@ -33,7 +33,78 @@ const Experience = () => {
               </ul>
             </div>
           </li>
-          
+          {/* <!--Third item--> */}
+          <li>
+            <div className='flex-start flex items-center pt-3'>
+              <div className='-ml-[5px] mr-3 h-[9px] w-[9px] rounded-full bg-neutral-300 dark:bg-neutral-500'></div>
+              <h1 className='text-xl font-bold text-[#6D83F2]'>Supersovu</h1>
+            </div>
+            <div className='mb-6 ml-4 mt-2'>
+              <button className='bg-gray-300 rounded-lg p-3'>Light Entrepreneur | May 2019 - Sep 2022 | Helsinki, Finland</button>
+              <ul>
+                <li>-	As a part-time chauffeur at Supersovu, I improved my interpersonal skills by interacting with diverse customers and managed financial responsibilities, providing income and expense information for monthly bookkeeping, VAT calculation, and tax reports.</li>
+              </ul>
+            </div>
+          </li>
+           {/* <!--Fourth item--> */}
+          <li>
+            <div className='flex-start flex items-center pt-3'>
+              <div className='-ml-[5px] mr-3 h-[9px] w-[9px] rounded-full bg-neutral-300 dark:bg-neutral-500'></div>
+              <h1 className='text-xl font-bold text-[#6D83F2]'>RTK Palvelut Oy</h1>
+            </div>
+            <div className='mb-6 ml-4 mt-2'>
+              <button className='bg-gray-300 rounded-lg p-3'>Team Lead | Vantaa, Finland | Aug 2020 - Dec 2020</button>
+              <ul>
+                <li>-	I contributed to daily housekeeping tasks and successfully managed team workflow, cultivating essential team management skills and ensuring high customer satisfaction.</li>
+              </ul>
+            </div>
+            <div className='mb-6 ml-4 mt-2'>
+              <button className='bg-gray-300 rounded-lg p-3'>Housekeeper | Vantaa, Finland | Aug 2019 - July 2020</button>
+              <ul>
+                <li>-	Performed cleaning tasks and maintained a sanitary environment, ensuring continuous availability of well-stocked supplies through regular replenishment.</li>
+              </ul>
+            </div>
+          </li>
+          {/* <!--Fifth item--> */}
+          <li>
+            <div className='flex-start flex items-center pt-3'>
+              <div className='-ml-[5px] mr-3 h-[9px] w-[9px] rounded-full bg-neutral-300 dark:bg-neutral-500'></div>
+              <h1 className='text-xl font-bold text-[#6D83F2]'>Sol Palvelut Oy</h1>
+            </div>
+            <div className='mb-6 ml-4 mt-2'>
+              <button className='bg-gray-300 rounded-lg p-3'>Housekeeper | May 2016 - July 2019 | Helsinki, Finland</button>
+              <ul>
+                <li>-	Performed housekeeping tasks in long-distance trains.</li>
+              </ul>
+            </div>
+          </li>
+          {/* <!--Sixth item--> */}
+          <li>
+            <div className='flex-start flex items-center pt-3'>
+              <div className='-ml-[5px] mr-3 h-[9px] w-[9px] rounded-full bg-neutral-300 dark:bg-neutral-500'></div>
+              <h1 className='text-xl font-bold text-[#6D83F2]'>Posti Oy (Former Itella)</h1>
+            </div>
+            <div className='mb-6 ml-4 mt-2'>
+              <button className='bg-gray-300 rounded-lg p-3'>Newspaper/Mail Deliverer | June 2011 - Nov 2018 | Helsinki, Finland</button>
+              <ul>
+                <li>-	Delivered early morning Helsingin Sanomat newspaper in different parts of Helsinki.</li>
+                <li>-	Delivered mails and small packages to households.</li>
+              </ul>
+            </div>
+          </li>
+          {/* <!--Seventh item--> */}
+          <li>
+            <div className='flex-start flex items-center pt-3'>
+              <div className='-ml-[5px] mr-3 h-[9px] w-[9px] rounded-full bg-neutral-300 dark:bg-neutral-500'></div>
+              <h1 className='text-xl font-bold text-[#6D83F2]'>Helsingin Jakelu-Expert Oy</h1>
+            </div>
+            <div className='mb-6 ml-4 mt-2'>
+              <button className='bg-gray-300 rounded-lg p-3'>Newspaper Sorter/Deliverer | Nov 2010 - July 2016 | Helsinki, Finland</button>
+              <ul>
+                <li>-	Sorted and delivered advertisement newspaper to households.</li>
+              </ul>
+            </div>
+          </li>
       </ol>
     </div>
   )
