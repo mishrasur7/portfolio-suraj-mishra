9df2938dc8da87a2d58239a815b1d37492976adb
Suraj Mishra <mishrasur7@gmail.com>
2025-01-24 07:52:35 +0200

    update file

--- src/components/experience/Experience.jsx
+++ src/components/experience/Experience.jsx
@@ -11,7 +11,7 @@ const Experience = () => {
               <h1 className='text-xl font-bold text-[#6D83F2]'>Vesko Ltd</h1>
             </div>
             <div className='mb-6 ml-4 mt-2'>
-              <button className='bg-gray-300 rounded-lg p-3'>Web Developer Intern | Oct 2024 - Dec 2024 | Remote</button>
+              <button className='bg-gray-300 rounded-lg p-3'>Web Developer Intern | Sep 2024 - Dec 2024 | Remote</button>
               <ul>
                 <li>-   Implementing features, components, and UI layouts with React TypeScript. </li>
                 <li>-	Migrated code from old codebase into new one, resulting in better management of component structure, features, and functionalities.</li>
