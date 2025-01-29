f0d0a49e0e9d16b252845c43e4a5411e4a85678a
Suraj Mishra <mishrasur7@gmail.com>
2025-01-29 17:46:51 +0200

    button changed to div

--- src/components/contact/Contact.jsx
+++ src/components/contact/Contact.jsx
@@ -4,7 +4,7 @@ const Contact = () => {
     <div id='contact' className='mt-24 scroll-m-20'>
         <h1 className='text-2xl text-center text-[#6D83F2] font-mono mb-10 '>Contact</h1>
         <div className='flex items-center justify-center'>
-            <button className='bg-yellow-300 rounded-xl p-14 font-mono'>+358 440778001 | mishrasur7@gmail.com</button>
+            <div className='bg-yellow-300 rounded-xl p-14 font-mono'>+358 440778001 | mishrasur7@gmail.com</div>
         </div>
     </div>
   )
--- src/components/experience/Experience.jsx
+++ src/components/experience/Experience.jsx
@@ -11,7 +11,7 @@ const Experience = () => {
               <h1 className='text-xl font-bold text-[#6D83F2]'>Vesko Ltd</h1>
             </div>
             <div className='mb-6 ml-4 mt-2'>
-              <button className='bg-gray-300 rounded-lg p-3'>Web Developer Intern | Sep 2024 - Dec 2024 | Remote</button>
+              <div className='bg-gray-300 rounded-lg p-3'>Web Developer Intern | Sep 2024 - Dec 2024 | Remote</div>
               <ul>
                 <li>-   Implementing features, components, and UI layouts with React TypeScript. </li>
                 <li>-	Migrated code from old codebase into new one, resulting in better management of component structure, features, and functionalities.</li>
@@ -26,7 +26,7 @@ const Experience = () => {
               <h1 className='text-xl font-bold text-[#6D83F2]'>Integrify Oy</h1>
             </div>
             <div className='mb-6 ml-4 mt-2'>
-              <button className='bg-gray-300 rounded-lg p-3'>Full Stack Developer Trainee | May 2022 - Oct 2022 | Helsinki, Finland</button>
+              <div className='bg-gray-300 rounded-lg p-3'>Full Stack Developer Trainee | May 2022 - Oct 2022 | Helsinki, Finland</div>
               <ul>
                 <li>-	Acquired in-depth knowledge and hands-on experience in full-stack web development, utilizing React, Redux, Express, Node, & MongoDB.</li>
                 <li>-	Actively engaged in daily problem-solving exercises, while effectively collaborating and communicating with a diverse team of developers. </li>
@@ -40,7 +40,7 @@ const Experience = () => {
               <h1 className='text-xl font-bold text-[#6D83F2]'>Supersovu</h1>
             </div>
             <div className='mb-6 ml-4 mt-2'>
-              <button className='bg-gray-300 rounded-lg p-3'>Light Entrepreneur | May 2019 - Sep 2022 | Helsinki, Finland</button>
+              <div className='bg-gray-300 rounded-lg p-3'>Light Entrepreneur | May 2019 - Sep 2022 | Helsinki, Finland</div>
               <ul>
                 <li>-	As a part-time chauffeur at Supersovu, I improved my interpersonal skills by interacting with diverse customers and managed financial responsibilities, providing income and expense information for monthly bookkeeping, VAT calculation, and tax reports.</li>
               </ul>
@@ -53,13 +53,13 @@ const Experience = () => {
               <h1 className='text-xl font-bold text-[#6D83F2]'>RTK Palvelut Oy</h1>
             </div>
             <div className='mb-6 ml-4 mt-2'>
-              <button className='bg-gray-300 rounded-lg p-3'>Team Lead | Vantaa, Finland | Aug 2020 - Dec 2020</button>
+              <div className='bg-gray-300 rounded-lg p-3'>Team Lead | Vantaa, Finland | Aug 2020 - Dec 2020</div>
               <ul>
                 <li>-	I contributed to daily housekeeping tasks and successfully managed team workflow, cultivating essential team management skills and ensuring high customer satisfaction.</li>
               </ul>
             </div>
             <div className='mb-6 ml-4 mt-2'>
-              <button className='bg-gray-300 rounded-lg p-3'>Housekeeper | Vantaa, Finland | Aug 2019 - July 2020</button>
+              <div className='bg-gray-300 rounded-lg p-3'>Housekeeper | Vantaa, Finland | Aug 2019 - July 2020</div>
               <ul>
                 <li>-	Performed cleaning tasks and maintained a sanitary environment, ensuring continuous availability of well-stocked supplies through regular replenishment.</li>
               </ul>
@@ -72,7 +72,7 @@ const Experience = () => {
               <h1 className='text-xl font-bold text-[#6D83F2]'>Sol Palvelut Oy</h1>
             </div>
             <div className='mb-6 ml-4 mt-2'>
-              <button className='bg-gray-300 rounded-lg p-3'>Housekeeper | May 2016 - July 2019 | Helsinki, Finland</button>
+              <div className='bg-gray-300 rounded-lg p-3'>Housekeeper | May 2016 - July 2019 | Helsinki, Finland</div>
               <ul>
                 <li>-	Performed housekeeping tasks in long-distance trains.</li>
               </ul>
@@ -85,7 +85,7 @@ const Experience = () => {
               <h1 className='text-xl font-bold text-[#6D83F2]'>Posti Oy (Former Itella)</h1>
             </div>
             <div className='mb-6 ml-4 mt-2'>
-              <button className='bg-gray-300 rounded-lg p-3'>Newspaper/Mail Deliverer | June 2011 - Nov 2018 | Helsinki, Finland</button>
+              <div className='bg-gray-300 rounded-lg p-3'>Newspaper/Mail Deliverer | June 2011 - Nov 2018 | Helsinki, Finland</div>
               <ul>
                 <li>-	Delivered early morning Helsingin Sanomat newspaper in different parts of Helsinki.</li>
                 <li>-	Delivered mails and small packages to households.</li>
@@ -99,7 +99,7 @@ const Experience = () => {
               <h1 className='text-xl font-bold text-[#6D83F2]'>OK Palvelut Oy</h1>
             </div>
             <div className='mb-6 ml-4 mt-2'>
-              <button className='bg-gray-300 rounded-lg p-3'>Construction Cleaner | April 2011 - May 2011 | Helsinki, Finland</button>
+              <div className='bg-gray-300 rounded-lg p-3'>Construction Cleaner | April 2011 - May 2011 | Helsinki, Finland</div>
               <ul>
                 <li>-	Performed cleaning activities in newly constructed residential and commercial buildings.</li>
               </ul>
@@ -112,7 +112,7 @@ const Experience = () => {
               <h1 className='text-xl font-bold text-[#6D83F2]'>Helsingin Jakelu-Expert Oy</h1>
             </div>
             <div className='mb-6 ml-4 mt-2'>
-              <button className='bg-gray-300 rounded-lg p-3'>Newspaper Sorter/Deliverer | Nov 2010 - July 2016 | Helsinki, Finland</button>
+              <div className='bg-gray-300 rounded-lg p-3'>Newspaper Sorter/Deliverer | Nov 2010 - July 2016 | Helsinki, Finland</div>
               <ul>
                 <li>-	Sorted and delivered advertisement newspaper to households.</li>
               </ul>
