f3dfa6c4fc83fd2839b71a82455103ff07141ba8
Suraj Mishra <mishrasur7@gmail.com>
2025-02-12 22:00:37 +0200

    update

--- src/components/experience/Experience.jsx
+++ src/components/experience/Experience.jsx
@@ -4,21 +4,6 @@ const Experience = () => {
     <div id='experience' className='mt-24 scroll-m-20'>
         <h1 className='text-2xl text-center text-[#6D83F2] font-mono mb-10 '>Work Experience</h1>
          <ol className='border-l-2 mx-4 sm:mx-48'>
-          {/* <!--First item--> */}
-          <li>
-            <div className='flex-start flex items-center pt-3'>
-              <div className='-ml-[5px] mr-3 h-[9px] w-[9px] rounded-full bg-neutral-300 dark:bg-neutral-500'></div>
-              <h1 className='text-xl font-bold text-[#6D83F2]'>Vesko Ltd</h1>
-            </div>
-            <div className='mb-6 ml-4 mt-2'>
-              <div className='bg-gray-300 rounded-lg p-3'>Web Developer Intern | Oct 2024 - Dec 2024 | Remote</div>
-              <ul>
-                <li>-   Implementing features, components, and UI layouts with React TypeScript. </li>
-                <li>-	Migrated code from old codebase into new one, resulting in better management of component structure, features, and functionalities.</li>
-                <li>-	Reduced development complexity for responsiveness and cross browser performance by 20% transitioning from Material UI to Tailwind CSS. </li>
-              </ul>
-            </div>
-          </li>
           {/* <!--Second item--> */}
            <li>
             <div className='flex-start flex items-center pt-3'>
