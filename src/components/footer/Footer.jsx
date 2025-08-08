ef62b68b3c6964c4fd4820e1a39192e2286147dd
Suraj Mishra <mishrasur7@gmail.com>
2025-08-08 10:17:17 +0300

    footer updated

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -2,8 +2,8 @@
 const Footer = () => {
   return (
     <div className='bg-gray-50 border-t-2 mt-24 h-16 flex justify-around items-center text-[#6A98F0]'>
-        <a href="#" className='font-mono hover:font-bold hover:text-xl'>&lt;SurajMishra/&gt;</a>
-        <p>Made with <span className='text-red-500 text-xl'>&hearts; </span><span>&copy; Jan 2025</span></p>
+        <a href="#" className='font-mono hover:font-bold hover:text-xl'>&lt;Suraj Mishra - Manufactured in Nepal, Shipped to Finland/&gt;</a>
+        <p>Made with <span className='text-red-500 text-xl'>&hearts; </span><span>&copy; 2025</span></p>
     </div>
   )
 }
